816bfd2e827d20ce93e5a236eb40fee19e689eb3
jing <1119577355@qq.com>
2018-11-22 21:31:56 +0800

    first

---
+++ js/js/1.js
@@ -0,0 +1,4 @@
+function tochange(){
+	var obox =document.getElementById('box');
+	obox.style.width="200px";
+}
---
+++ js/js/2.js
@@ -0,0 +1,7 @@
+function tochange(){
+	var obox =document.getElementById('box');
+	obox.style.width="200px";
+}
+
+var obox=document.getElementById('box');
+obox.onclick=tochange
